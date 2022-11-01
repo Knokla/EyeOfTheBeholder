2e16cdbf0f0269c4c0b43f5e701b805083d75593
www0rker <www0rker@mac.local>
2022-11-01 16:19:10 +0500

    minor polish

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import { createServer } from 'http';
 
 export type LiveReport = {
-    time: number;
-    payloadLength: number;
-    idleTime: number;
-    lastIn: number;
-    lastOut: number;
-    startTime: number;
-    extra: any;
+    time?: number;
+    payloadLength?: number;
+    idleTime?: number;
+    lastIn?: number;
+    lastOut?: number;
+    startTime?: number;
+    extra?: any;
 }
 
 export default class Eye {
-    private data: LiveReport;
-    constructor(port: number) {
+    private data: LiveReport = { startTime: Date.now() };
+    constructor(route: string, port: number) {
         const server = createServer((req, res) => {
             res.setHeader('Content-Type', 'application/json; charset=UTF-8');
             const baseUrl = req.url.split('?')[0];
             switch (baseUrl) {
-                case '/getData': res.end(JSON.stringify(this.data)); break;
+                case route: res.end(JSON.stringify(this.data)); break;
                 default: res.end(`{ "status": "error", "msg": "unknown url" }`);
             }
         });
@@ -26,18 +26,18 @@ export default class Eye {
 
     public setData = (data: LiveReport, ...args) => {
         this.data = { ...data };
+        this.data.time = Date.now();
         this.data.extra = { ...args[0] };
     }
 };
 
-// const eye = new Eye(7401);
+// const eye = new Eye('/getTelemetry', 7401);
 // const data = {
 //     time: 111,
 //     payloadLength: 222,
 //     idleTime: 333,
 //     lastIn: 4444,
 //     lastOut: 555,
-//     startTime: 666,
 //     extra: {some: 1, else: 2}
 // }
 // eye.setData(data, {test: 1, test2: 2, test3: 3});
